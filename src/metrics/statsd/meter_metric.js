ae2d90daf0272158c5acd3136d70fd5a08cc08ce
Raffaele <raffaelecalza4@gmail.com>
2021-09-11 20:23:35 +0200

    removed statsd metrics types

--- src/metrics/statsd/meter_metric.js
+++
@@ -1,9 +0,0 @@
-var AbstractMetric = require('./abstract_statsd_metric.js'),
-    util = require('util');
-
-function MeterMetric (opts) {
-  AbstractMetric.call(this, opts, 'm');
-}
-util.inherits(MeterMetric, AbstractMetric);
-
-module.exports = MeterMetric;
--- src/metrics/statsd/timer_metric.js
+++
@@ -1,9 +0,0 @@
-var AbstractMetric = require('./abstract_statsd_metric.js'),
-    util = require('util');
-
-function TimerMetric (opts) {
-  AbstractMetric.call(this, opts, 'ms');
-}
-util.inherits(TimerMetric, AbstractMetric);
-
-module.exports = TimerMetric;
