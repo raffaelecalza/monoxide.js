4fd6f0324ea56fb927b4ad7d77b11c4012231960
Raffaele <raffaelecalza4@gmail.com>
2021-09-21 16:18:21 +0200

    linted carbon example

--- examples/carbon_client.js
+++ examples/carbon_client.js
@@ -1,24 +1,24 @@
 const Monoxide = require('../src')
 
-let config = new Monoxide.Config('127.0.0.1', 2003, 10000, 'example.carbon.client')
-let protocol = new Monoxide.Protocols.PlainTextProtocol()
+const config = new Monoxide.Config('127.0.0.1', 2003, 10000, 'example.carbon.client')
+const protocol = new Monoxide.Protocols.PlainTextProtocol()
 
-let client = new Monoxide.Clients.CarbonClient(config, protocol)
+const client = new Monoxide.Clients.CarbonClient(config, protocol)
 
-//client.connect()
+// client.connect()
 client.start()
 client.on('error', (err) => {
-    console.log('There was an error')
-    console.log(err)
+  console.log('There was an error')
+  console.log(err)
 })
 client.on('metrics-sent', () => {
-    console.log('Metrics correctly sent to Carbon')
+  console.log('Metrics correctly sent to Carbon')
 })
 
-client.record('metric.1', 20, new Date())
-client.record('metric.2', 30, new Date())
-client.record('metric.3', 10, new Date())
+client.record('metric.7', 20, new Date())
+client.record('metric.9', 30, new Date())
+client.record('metric.8', 10, new Date())
 
 setTimeout(() => {
-    client.stop()
+  client.stop()
 }, 15000)
